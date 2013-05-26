f2a62faab36734628a459b8e5eacd5c0370a9172
yuki2006 <yagfair@gmail.com>
2013-05-26 16:28:17 +0900

    add SRM 580 Div2 Easy/Middle

--- src/WidgetRepairs.java
+++ src/WidgetRepairs.java
@@ -1,31 +1,18 @@
 //SRM150 DIV2 250
 public class WidgetRepairs {
 	public int days(int[] arrivals, int numPerDay) {
-		int how = 0;
+		int day = 0;
 		for (int i = 0; i < arrivals.length - 1; i++) {
-			if (arrivals[i] / numPerDay == 0 && arrivals[i] % numPerDay > 0) {
-				how++;
-			} else {
-				how += arrivals[i] / numPerDay;
-				arrivals[i + 1] += arrivals[i] % numPerDay;
+			if (arrivals[i] > 0) {
+				day++;
+				arrivals[i + 1] +=Math.max(arrivals[i] - numPerDay,0);
 			}
 		}
+		day += arrivals[arrivals.length - 1] / numPerDay;
 		if (arrivals[arrivals.length - 1] % numPerDay > 0) {
-			how += arrivals[arrivals.length - 1] / numPerDay + 1;
-		} else {
-			how += arrivals[arrivals.length - 1] / numPerDay;
+			day++;
 		}
-		return how;
-	}
-
-	// BEGIN CUT HERE
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			WidgetRepairsHarness.run_test(-1);
-		} else {
-			for (int i = 0; i < args.length; ++i)
-				WidgetRepairsHarness.run_test(Integer.valueOf(args[i]));
-		}
+		return day;
 	}
 	// END CUT HERE
 }
